709e0f8f584573c8108e7612f9f92e4d0f5d10c7
chujacky <jacky9407@gmail.com>
2018-12-19 19:57:29 -0800

    Install webpack and set up the skeleton of the module

---
+++ client/src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var App = (props) => {
+  return (
+  <div>
+    <h1>Hi</h1>
+  </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('main'));
