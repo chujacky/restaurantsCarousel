7cdb596082695774c659056dc460c779946f7273
chujacky <jacky9407@gmail.com>
2018-12-19 22:24:35 -0800

    Create server

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const db = require('../db/index.js');
+const app = express();
+const port = 3002;
+
+
+app.use(express.static(path.join(__dirname, '../client/dist')))
+
+app.listen(port, () => {console.log('Server is running')})
+
