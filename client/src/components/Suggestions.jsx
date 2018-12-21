849b89e8d32f8ccad33c6267b2e2795c5a48602b
chujacky <jacky9407@gmail.com>
2018-12-21 14:27:53 -0800

    Fix codes format

--- client/src/components/Suggestions.jsx
+++ client/src/components/Suggestions.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import SuggestionList from './SuggestionList.jsx';
 
-class App extends React.Component {
+class Suggestions extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -11,8 +11,7 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    const id = window.location.pathname.substring(13);
-    axios.get(`/restaurants/${id}/suggestions`)
+    axios.get(`/restaurants/${this.props.id}/suggestions`)
       .then((response) => {
         console.log(response.data);
         this.setState({
@@ -33,4 +32,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default Suggestions;
