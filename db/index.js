c3557d2f9192c1a39e271210492926fbdd0eef6c
chujacky <jacky9407@gmail.com>
2018-12-20 22:10:59 -0800

    Implement first API call for sample data

--- db/index.js
+++ db/index.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Promise = require('bluebird');
 mongoose.connect('mongodb://localhost/grubhub');
 
 const suggestionSchema = new mongoose.Schema({
@@ -17,4 +18,28 @@ const suggestionSchema = new mongoose.Schema({
 
 const Suggestion = mongoose.model('Suggestion', suggestionSchema);
 
-module.exports = Suggestion;
+const get = (id, callback) => {
+  Suggestion.find({ id:id }, (err, data) => {
+    if (err) {
+      callback(err);
+      return;
+    }
+    let restaurants = [];
+    console.log(data[0].suggestions)
+    for (let i = 0; i < data[0].suggestions.length; i++) {
+      restaurants.push(Suggestion.find({ id: data[0].suggestions[i] }));
+    }
+    Promise.all(restaurants)
+      .then((values) => {
+        callback(null, values);
+      })
+      .catch((err) => {
+        callback(err);
+      });
+  });
+};
+
+module.exports = {
+  Suggestion,
+  get,
+};
--- server/index.js
+++ server/index.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const db = require('../db/index.js');
 const app = express();
 const PORT = 3002;
 
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.get('/restaurants/:id', (req, res) => {
+  res.status(201).sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+});
+app.get('/restaurants/:id/suggestions', (req, res) => {
+  db.get(req.params.id, (err, data) => {
+    if (err) {
+      throw err;
+    }
+    res.status(200).json(data);
+  });
+});
 
 app.listen(PORT, () => { console.log(`listening on port ${PORT}`); });
