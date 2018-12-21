f1595dec575ea8cd3aa500238786d57b7ecefad0
chujacky <jacky9407@gmail.com>
2018-12-20 16:06:17 -0800

    Seed database

---
+++ db/seeding.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Suggestion = require('./index.js');
+const faker = require('faker');
+
+for (let i = 1; i < 101; i++) {
+  let suggestions = [];
+  for (let j = 0; j < 10; j++) {
+    suggestions.push(faker.random.number(100));
+  }
+  const restaurant = new Suggestion({
+    id: i,
+    name: faker.name.findName(),
+    address: faker.address.streetAddress(),
+    waiting_time: faker.random.number(60),
+    minimum: faker.random.number(15),
+    review_no: faker.random.number(2000),
+    review_summary: {
+      stars: faker.random.number(5),
+      good: faker.random.number(100),
+      onTime: faker.random.number({
+        min: 60,
+        max: 100,
+      }),
+      accurate: faker.random.number({
+        min: 70,
+        max: 100,
+      }),
+    },
+    picture: faker.image.food(),
+    suggestions,
+    bookmarked: faker.random.boolean(),
+  });
+  restaurant.save();
+}
