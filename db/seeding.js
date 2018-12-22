d68ab243013fffe58d3543dfb392989216672b1b
chujacky <jacky9407@gmail.com>
2018-12-21 17:07:57 -0800

    Update sameple data images and fix code styles

--- db/seeding.js
+++ db/seeding.js
@@ -1,10 +1,19 @@
 const mongoose = require('mongoose');
-const {Suggestion} = require('./index.js');
+const { Suggestion } = require('./index.js');
 const faker = require('faker');
 
-for (let i = 1; i < 101; i++) {
-  let suggestions = [];
-  for (let j = 0; j < 12; j++) {
+const pictures = ['https://s.hdnux.com/photos/72/15/17/15350667/7/premium_landscape.jpg', 'https://s.hdnux.com/photos/72/15/17/15352415/7/premium_landscape.jpg', 'https://s.hdnux.com/photos/72/15/17/15346423/7/premium_landscape.jpg',
+  'https://s.hdnux.com/photos/72/15/17/15347780/7/premium_landscape.jpg', 'https://s.hdnux.com/photos/72/15/17/15351888/7/premium_landscape.jpg', 'https://s.hdnux.com/photos/72/15/17/15351104/7/premium_landscape.jpg',
+  'https://s.hdnux.com/photos/72/15/17/15346491/7/premium_landscape.jpg', 'https://s.hdnux.com/photos/72/15/17/15346499/7/premium_landscape.jpg', 'https://s.hdnux.com/photos/72/15/17/15347796/7/premium_landscape.jpg',
+  'https://s.hdnux.com/photos/72/15/17/15346491/7/premium_landscape.jpg', 'https://s.hdnux.com/photos/72/15/17/15352160/7/premium_landscape.jpg', 'https://s.hdnux.com/photos/72/15/17/15352126/7/premium_landscape.jpg',
+  'https://s.hdnux.com/photos/72/15/17/15351111/7/premium_landscape.jpg', 'https://s.hdnux.com/photos/72/15/17/15351873/7/premium_landscape.jpg', 'https://s.hdnux.com/photos/72/15/17/15351099/7/premium_landscape.jpg',
+  'https://s.hdnux.com/photos/72/15/17/15351870/7/premium_landscape.jpg', 'https://s.hdnux.com/photos/72/15/17/15351258/7/premium_landscape.jpg', 'https://s.hdnux.com/photos/72/15/17/15352488/7/premium_landscape.jpg',
+  'https://s.hdnux.com/photos/72/15/17/15351087/7/premium_landscape.jpg', 'https://s.hdnux.com/photos/72/15/17/15347660/7/premium_landscape.jpg', 'https://s.hdnux.com/photos/72/15/17/15346506/7/premium_landscape.jpg',
+];
+
+for (let i = 1; i < 101; i += 1) {
+  const suggestions = [];
+  for (let j = 0; j < 12; j += 1) {
     suggestions.push(faker.random.number(100));
   }
   const restaurant = new Suggestion({
@@ -30,7 +39,7 @@ for (let i = 1; i < 101; i++) {
       username: faker.name.firstName(),
       review: faker.lorem.sentence(),
     },
-    picture: faker.image.food(),
+    picture: pictures[faker.random.number(pictures.length - 1)],
     suggestions,
     bookmarked: faker.random.boolean(),
   });
